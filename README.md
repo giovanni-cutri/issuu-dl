1fa01f8566984e395d27ac60b55ec386befcd28f
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2022-08-20 20:13:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Issuu-Downloader
+ 
---
+++ main.py
@@ -0,0 +1,26 @@
+import os, requests, bs4, lxml, re, urllib.request
+
+current_dir = os.getcwd()
+
+print("Paste the link of the publication you want to download:")
+link = input()
+
+res = requests.get(link)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, "lxml")
+
+publication_name_raw = soup.select("meta[property='og:title']")[0].attrs["content"]
+publication_name = publication_name_raw.replace("/", " ").replace(":", "").replace("*", " ").replace("?", "")\
+    .replace("'", "").replace("<", "").replace(">", "").replace("|", "")
+
+publication_dir = os.path.join(current_dir, "publications", publication_name, "")
+os.makedirs(publication_dir)
+
+images_link = soup.select("meta[property='og:image']")[0].attrs["content"][:-5]
+pages_number = int(re.findall(re.compile("pageCount.*?,&quot"), res.text)[0][:-6][16:])
+
+for i in range(1, pages_number+1):
+    print("Saving page " + str(i) + " of " + str(pages_number) + "...")
+    urllib.request.urlretrieve(images_link + str(i) + ".jpg", publication_dir + str(i) + ".jpg")
+
+print("Done.")
